972068b0f654f369903a74a2a0d4dc5ab3ef1fbc
qiliq <49714834+qiliq@users.noreply.github.com>
2020-07-27 14:05:58 +0800

    Create Multiprocess.py

---
+++ Multiprocess.py
@@ -0,0 +1,72 @@
+# # 多进程
+# from multiprocessing import Process
+# import os
+#
+#
+# # 子进程要执行的代码
+# def run_proc(name):
+#     print('Run child process %s (%s)...' % (name, os.getpid()))
+#
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Process(target=run_proc, args=('test',))
+#     print('Child process will start.')
+#     p.start()
+#     p.join()
+#     print('Child process end.')
+#
+#
+# from multiprocessing import Pool
+# import os, time, random
+#
+#
+# def long_time_task(name):
+#     print('Run task %s (%s)...' % (name, os.getpid()))
+#     start = time.time()
+#     time.sleep(random.random() * 3)
+#     end = time.time()
+#     print('Task %s runs %0.2f seconds.' % (name, (end - start)))
+#
+# if __name__ == '__main__':
+#     print('Parent process %s.' % os.getpid())
+#     p = Pool(4)
+#     for i in range(5):
+#         p.apply_async(long_time_task, args=(i,))
+#     print('Waiting for all subprocesses done...')
+#     p.close()
+#     p.join()
+#     print('All subprocesses done.')
+
+# # 创建子进程
+# import subprocess
+#
+# print('$ nslookup www.python.org')
+# r = subprocess.call(['nslookup', 'www.python.com'])
+# print('Exit code:', r)
+
+
+# 创建线程
+import time
+import threading
+
+
+# 新线程执行的代码
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s ended.' % threading.current_thread().name)
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
+
+
+# 多进程中，同一个变量，各自有一份拷贝存在于每个进程中，互不影响，而多线程中，所有变量都由所有线程共享
+# 线程之间共享数据最大的危险在于多个线程同时改一个变量
+
